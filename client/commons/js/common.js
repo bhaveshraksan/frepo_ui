d6955ed9ebb2d205ae60f51f685195d05f90ecd0
chanduRaksan <chandrashekar.turupati@raksan.in>
2018-02-05 12:45:00 +0530

    Common js added

---
+++ client/commons/js/common.js
@@ -0,0 +1,48 @@
+
+    $(document).ready(function(){
+        $('.footer_arrow').click(function(){
+          $("footer").toggleClass("footer_close");
+          $(this).parents('footer').find(".footer_arrow span").toggleClass('fa-angle-double-down fa-angle-double-up');
+        });
+
+        $(function () { 
+$('[data-toggle="tooltip"]').tooltip({trigger: 'manual'}).tooltip('show');
+}); 
+
+// $( window ).scroll(function() { 
+// if($( window ).scrollTop() > 10){ // scroll down abit and get the action 
+$(".progress-bar").each(function(){
+each_bar_width = $(this).attr('aria-valuenow');
+$(this).width(each_bar_width + '%');
+});
+
+          var mySwiper = new Swiper('.swiper-month', {
+            speed: 400,
+            loop: true,
+            slidesPerView:'auto',
+            mousewheelControl:true
+          });
+
+          var swiper = new Swiper('.swiper-day', {
+            slidesPerView: 7,
+            spaceBetween: 10,
+            loop: true,
+            centeredSlides: true,
+          });
+
+
+      });
+
+
+
+      document.addEventListener('DOMContentLoaded', function(){
+  let body = document.querySelector('body');
+  document.querySelector('.themeselect').addEventListener('change', handleThemeSelect);
+  function handleThemeSelect(event) {
+    event.preventDefault();
+    
+    // change body class
+    body.className = event.target.value;
+  };
+  
+});
