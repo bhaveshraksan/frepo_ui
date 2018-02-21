f15a8f0a602ecf31928944d137d24545ec4cf68d
Damini <daminisinha64@gmail.com>
2018-02-21 15:23:27 +0530

    start button changes

--- client/views/frUsers/frLevel1/frAppointmentListing/frAppointmentListing.js
+++ client/views/frUsers/frLevel1/frAppointmentListing/frAppointmentListing.js
@@ -7,5 +7,13 @@ Template.frAppointmentListing.onRendered(function () {
          // $(".is_open").toggleClass(".sideOverlay_close");
           $(this).parents('.sideOverlay').find(".side_arrow span").toggleClass('fa-chevron-left fa-chevron-right');
         });
+        $('.startTrip').click(function(){
+           $('.startTrip').hide();
+           $('.endTrip').show().css("display","block");
+        });
+        $('.endTrip').click(function(){
+            $('.endTrip').hide();
+            $('.startTrip').show();
+         });
     });
 });
