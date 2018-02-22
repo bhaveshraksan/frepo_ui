15d2e25c25e236a759fe262d9f867ae2a993fcf4
Damini <daminisinha64@gmail.com>
2018-02-22 21:12:07 +0530

    changes related to jquery for appointment listing

--- client/views/frUsers/frLevel1/frAppointmentListing/frAppointmentListing.js
+++ client/views/frUsers/frLevel1/frAppointmentListing/frAppointmentListing.js
@@ -1,12 +1,15 @@
 Template.frAppointmentListing.onRendered(function () {
     $(document).ready(function(){
-        $('.side_arrow').click(function(){
-         $(".sideOverlay").removeClass("sideOverlay_close");
-        $(".sideOverlay").toggleClass("is_open");
-         
-         // $(".is_open").toggleClass(".sideOverlay_close");
-          $(this).parents('.sideOverlay').find(".side_arrow span").toggleClass('fa-chevron-left fa-chevron-right');
+        $(".sideOverlay").each(function (e) {
+            $(this).bind("click", function (e) {
+                var id = $(e.currentTarget).attr("id");
+                //alert(x)
+        //$('.side_arrow').click(function(){
+         $("#"+id).removeClass("sideOverlay_close");
+        $("#"+id).toggleClass("is_open");
+          $(this).parents(".sideOverlay").find(".side_arrow span").toggleClass('fa-chevron-left fa-chevron-right');
         });
+    });
         $('.startTrip').click(function(){
            $('.startTrip').hide();
            $('.endTrip').show().css("display","block");
@@ -15,5 +18,6 @@ Template.frAppointmentListing.onRendered(function () {
             $('.endTrip').hide();
             $('.startTrip').show();
          });
-    });
+   
+});
 });
